546d9a79dcc7308eb418bca279819ba3371b26eb
RonanEKane <ronanekane@gmail.com>
2025-12-16 11:44:01 -0500

    Delete src directory

--- src/firebase/config.js
+++
@@ -1,7 +0,0 @@
-// Firebase configuration stub
-// Currently not using Firebase backend - all data stored in localStorage
-
-export const initializeFirebase = () => {
-  // Stub function for future Firebase integration
-  console.log('Firebase stub initialized');
-};
