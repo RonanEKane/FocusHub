e4fc580196871ad95970881f72dd4a1c46756aeb
RonanEKane <ronanekane@gmail.com>
2026-01-20 15:46:04 -0500

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 // Supabase Configuration
-const SUPABASE_URL = 'https://zpbzursxjlhizminvyd.supabase.co';
+const SUPABASE_URL = 'https://zpbzursxjlhizminfvyd.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpwYnp1cnN4amxoaXptaW5mdnlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY3OTIzNjcsImV4cCI6MjA4MjM2ODM2N30.fKAO3lO5NEa2M-fHQC7I6uTb00rITdCA_o6Cek0H3Nk';
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
